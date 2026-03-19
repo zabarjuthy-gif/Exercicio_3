17389f4f1b3880cb4d5877e2ffd568bc441043e1
Zabarjuthy Mamudo <zabarjuthy@gmail.com>
2026-03-19 13:30:23 +0000

    comentario do commit

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -8,18 +8,36 @@ robot = Robot()
 # Get pointer to each wheel of our robot.
 leftWheel = robot.getDevice('left wheel')
 rightWheel = robot.getDevice('right wheel')
+rightWheelSensor = robot.getPositionSensor('right wheel sensor')
+rightWheelSensor.enable(16) # Refreshes the sensor every 16ms.
 
+diametro_roda = 0.195
+lado_quadrado = 2
+#Calculo do angulo a ser percorrido pela roda na linha recta
+angulo_linha_recta = 2*lado_quadrado/diametro_roda
+#Calculo do angulo a ser percorrido pela roda na viragem
+percurso_roda_viragem = 1/4*3.141592*(0.33/2)
+angulo_viragem = 2*percurso_roda_viragem/diametro_roda
+robot.step(16)
 # Repeat the following 4 times (once for each side).
 for i in range(0, 4):
+    valor_inicial_roda_direita = rightWheelSensor.getValue()
+    #print("Angulo inicial direita: %f"%valor_inicial_roda_direita)
     # First set both wheels to go forward, so the robot goes straight.
     leftWheel.setPosition(1000)
     rightWheel.setPosition(1000)
     # Wait for the robot to reach a corner.
-    robot.step(3900)
-    # Then, set the right wheel backward, so the robot will turn right.
+    # Malha aberta - temporizador
+    #robot.step(3900)
+    # Malha fechada - trajetoria em linha recta
+    valor_actual_roda_direita = rightWheelSensor.getValue()
+    while valor_actual_roda_direita - valor_inicial_roda_direita <= angulo_linha_recta:
+        valor_actual_roda_direita = rightWheelSensor.getValue()
+        robot.step(16)
+    # Virar a direita.
     leftWheel.setPosition(1000)
     rightWheel.setPosition(-1000)
-    # Wait until the robot has turned 90 degrees clockwise.
+    # Malha aberta - temporizador para virar a direita
     robot.step(480)
 
 # Stop the robot when path is completed, as the robot performance
